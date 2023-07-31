7ba9e9136ff51545c725ee2861e65113c516e171
AhnDo0 <i0796075@gmail.com>
2023-07-31 13:51:56 +0900

    refactor: 코드 스타일 수정

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,14 +1,13 @@
 import { AdminFooter, AdminHeader } from "../components";
 
-
 function Test() {
-    return(
-        <>
-            <AdminHeader/>
-            <h1>Test</h1>
-            <AdminFooter/>
-        </>
-    );
+  return (
+    <>
+      <AdminHeader />
+      <h1>Test</h1>
+      <AdminFooter />
+    </>
+  );
 }
 
 export default Test;
